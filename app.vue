ee7180710f72bf6b195fac7cc22a66008a79af01
yuxxeun <itsyuxxeun@gmail.com>
2022-10-07 23:42:35 +0700

    nitro!

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component. </AppAlert>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const runtimeConfig = useRuntimeConfig()
+</script>
+
+
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component </AppAlert>
+  </div>
+</template>
